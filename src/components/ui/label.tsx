ee8419bafbe74d364f0542749c4692300c914620
Craig Rosario <craig.rosario04@gmail.com>
2025-06-11 08:29:56 +0530

    Added external tailwind reference to shadcn components

--- src/components/ui/label.tsx
+++ src/components/ui/label.tsx
@@ -2,6 +2,7 @@ import * as React from "react"
 import * as LabelPrimitive from "@radix-ui/react-label"
 
 import { cn } from "@/lib/utils"
+import { labelRootState } from "../styles/label"
 
 function Label({
   className,
@@ -11,7 +12,7 @@ function Label({
     <LabelPrimitive.Root
       data-slot="label"
       className={cn(
-        "flex items-center gap-2 text-sm leading-none font-medium select-none group-data-[disabled=true]:pointer-events-none group-data-[disabled=true]:opacity-50 peer-disabled:cursor-not-allowed peer-disabled:opacity-50",
+        labelRootState,
         className
       )}
       {...props}
